e41989f32ebdb86381848d820d7c34dc4de5dc2a
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-07-14 18:34:00 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,23 @@
 def consolidate_cart(cart)
   # code here
+  consolidated_cart = {}
+  cart.each do |hash_of_items| #iterate through the hash_of_items
+    hash_of_items.each do |item, value| #item is key value is value which is hash of paramters
+      if !consolidated_cart.has_key? (item)
+        consolidated_cart[item] = {}
+        consolidated_cart[item][count] = 0 
+      else 
+        consolidated_cart[item][count]+=1 #if it's not in the consolidated cart, put it in and count how many times it's in there.
+      end
+      
+      value.each do |parameter, value_of_parameter|
+        if !consolidated_cart[item].has_key? (parameter)
+          consolidated_cart[item][parameter] = value_of_parameter
+        end 
+      end
+    end 
+  end 
+  return consolidated_cart
 end
 
 def apply_coupons(cart, coupons)
